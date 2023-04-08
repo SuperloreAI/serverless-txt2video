fb82241d404aed198e0daceb0be612e8ceb30c95
0xnewton-superlore <0xnewton.superlore@gmail.com>
2023-04-07 22:38:28 -0400

    first commit - ported over

---
+++ Dockerfile
@@ -0,0 +1,65 @@
+FROM nvidia/cuda:11.7.1-runtime-ubuntu22.04
+  
+# To use a different model, change the model URL below:
+# Real model url https://huggingface.co/damo-vilab/modelscope-damo-text-to-video-synthesis
+# ARG MODEL_URL='https://huggingface.co/runwayml/stable-diffusion-v1-5/blob/main/v1-5-pruned-emaonly.ckpt'
+# Array representation in a string of controlnet models
+# Use a comma to separate the models [IMPORTANT]
+ARG MODEL_URLS="\
+    https://huggingface.co/damo-vilab/modelscope-damo-text-to-video-synthesis/blob/main/VQGAN_autoencoder.pth,\
+    https://huggingface.co/damo-vilab/modelscope-damo-text-to-video-synthesis/blob/main/text2video_pytorch_model.pth,\
+    https://huggingface.co/damo-vilab/modelscope-damo-text-to-video-synthesis/blob/main/configuration.json,\
+    https://huggingface.co/damo-vilab/modelscope-damo-text-to-video-synthesis/blob/main/open_clip_pytorch_model.bin"
+
+# If you are using a private Huggingface model (sign in required to download) insert your Huggingface
+# access token (https://huggingface.co/settings/tokens) below:
+ARG HF_TOKEN=''
+
+ARG GCP_SERVICE_ACCOUNT_JSON=''
+
+RUN apt update && apt-get -y install git wget \
+    python3.10 python3.10-venv python3-pip \
+    build-essential libgl-dev libglib2.0-0 vim \
+    ffmpeg
+RUN ln -s /usr/bin/python3.10 /usr/bin/python
+
+RUN useradd -ms /bin/bash banana
+
+WORKDIR /app
+
+# used to store temporary files
+RUN mkdir -p /app/temp_work_files
+
+# SD Webui
+RUN git clone https://github.com/SuperloreAI/stable-diffusion-webui.git && \
+    cd stable-diffusion-webui && \
+    git checkout 769def1e418c74107e4bfe1c7c990d20faed4c17
+
+WORKDIR /app/stable-diffusion-webui
+
+# Download the txt2video stuff
+RUN git clone https://github.com/SuperloreAI/sd-webui-text2video.git extensions/sd-webui-text2video && \
+    cd extensions/sd-webui-text2video && \
+    git checkout e38b3e82369f4eaf9bd383ea0a9969a86f981922
+    
+RUN mkdir -p models/ModelScope/t2v
+
+ENV MODEL_URLS=${MODEL_URLS}
+ENV HF_TOKEN=${HF_TOKEN}
+
+RUN pip install tqdm requests
+ADD download_models.py .
+RUN python download_models.py
+
+ADD prepare.py .
+RUN python prepare.py --skip-torch-cuda-test --xformers --reinstall-torch --reinstall-xformers
+
+ADD download.py download.py
+RUN python download.py --use-cpu=all
+
+RUN mkdir -p extensions/banana/scripts
+ADD script.py extensions/banana/scripts/banana.py
+ADD app.py app.py
+ADD server.py server.py
+
+CMD ["python", "server.py", "--xformers", "--disable-safe-unpickle", "--lowram", "--no-hashing", "--listen", "--port", "8000"]
---
+++ download_models.py
@@ -0,0 +1,81 @@
+import os
+import requests
+import sys
+import time
+from tqdm import tqdm
+from typing import Union
+from urllib.parse import urlparse
+
+MODEL_URLS = os.environ.get('MODEL_URLS')
+HF_TOKEN = os.environ.get('HF_TOKEN', '')
+
+CHUNK_SIZE = 1024 * 1024
+
+def get_filename(model_url, id="model", path="models/Stable-diffusion/"):
+    if '.safetensors' in model_url:
+        return path + id + '.safetensors'
+    if '.pth' in model_url:
+        return path + id + '.pth'
+    else:
+        return path + id + '.ckpt'
+
+def check_model_file(filename):
+    # file_size_mb = round(os.path.getsize(filename) / (1024 * 1024))
+    # if file_size_mb < 100:
+    #     print(f'The downloaded file is only {file_size_mb} MB and does not appear to be a valid model.')
+    #     sys.exit(1)
+    return
+
+def download_hf_file(model_url, HF_TOKEN, id="model", path="models/Stable-diffusion/"):
+    filename = get_filename(model_url, id, path)
+    if os.path.exists(filename):
+        return
+    print("Model URL:", model_url)
+    print("Download Location:", filename)
+    if not HF_TOKEN:
+        print("A Huggingface token was not provided.")
+    else:
+        print("Using Huggingface authentication token.")
+    headers = {'Authorization': f'Bearer {HF_TOKEN}'}
+    response = requests.get(model_url, headers=headers, stream=True)
+    response.raise_for_status()
+    with open(filename, 'wb') as f, tqdm(desc="Downloading", unit="bytes", total=int(response.headers.get('content-length', 0))) as progress:
+        for chunk in response.iter_content(chunk_size=CHUNK_SIZE):
+            if chunk:
+                f.write(chunk)
+                progress.update(len(chunk))
+    check_model_file(filename)
+
+def download_other_file(model_url, id="model", path="models/Stable-diffusion/"):
+    filename = get_filename(model_url, id, path)
+    if os.path.exists(filename):
+        return
+    print("Model URL:", model_url)
+    print("Download Location:", filename)
+    response = requests.get(model_url, stream=True)
+    response.raise_for_status()
+    with open(filename, 'wb') as f, tqdm(desc="Downloading", unit="bytes", total=int(response.headers.get('content-length', 0))) as progress:
+        for chunk in response.iter_content(chunk_size=CHUNK_SIZE):
+            if chunk:
+                f.write(chunk)
+                progress.update(len(chunk))
+    check_model_file(filename)
+
+def download(url, id="model", path="models/Stable-diffusion/"):
+    if 'huggingface.co' in url:
+        if '/blob/' in url:
+            url = url.replace('/blob/', '/resolve/')
+        download_hf_file(url, HF_TOKEN, id, path)
+    else:
+        download_other_file(url, id, path)
+
+if __name__ == '__main__':
+    for i, url in enumerate(MODEL_URLS):
+        print('Downloading model', i, url)
+        # Model name becomes the model ID
+        # Get the filename with extension from the URL
+        filename_with_ext = os.path.basename(urlparse(url).path)
+
+        # Get the filename without extension
+        filename_without_ext, _ = os.path.splitext(filename_with_ext)
+        download(url, filename_without_ext, path="models/ModelScope/t2v")
